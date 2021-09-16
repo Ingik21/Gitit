23500df94f6a786c8fb7a0a14e56d9d17946ec3c
Ingik21 <89972195+Ingik21@users.noreply.github.com>
2021-09-16 09:46:31 +0000

    Update tile_traveller.py

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,1 +1,19 @@
 
+from types import WrapperDescriptorType
+
+
+N = North
+W = West
+S = South
+E = East
+
+def location():
+    tile_1_1 = North
+    tile_1_2 = North, South, East
+    tile_1_3 = South, East
+    tile_2_1 = North
+    tile_2_2 = South, West
+    tile_2_3 = East, West
+    tile_3_3 = South, West 
+    tile_3_2 = North, South
+    tile_3_3 = Victory
