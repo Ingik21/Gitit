4d5c37026d0d971255eb30b6f0606583af2d2fa8
Ingik21 <89972195+Ingik21@users.noreply.github.com>
2021-09-16 09:49:37 +0000

    Update tile_traveller.py

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,16 +1,17 @@
+N = (n)North
+W = (s)West
+S = (s)South
+E = (e)East
 
-N = North
-W = West
-S = South
-E = East
+tile_1_1 = North
+tile_1_2 = North, South, East
+tile_1_3 = South, East
+tile_2_1 = North
+tile_2_2 = South, West
+tile_2_3 = East, West
+tile_3_3 = South, West 
+tile_3_2 = North, South
+tile_3_3 = Victory
+starting = tile_1_1
 
-def location():
-    tile_1_1 = North
-    tile_1_2 = North, South, East
-    tile_1_3 = South, East
-    tile_2_1 = North
-    tile_2_2 = South, West
-    tile_2_3 = East, West
-    tile_3_3 = South, West 
-    tile_3_2 = North, South
-    tile_3_3 = Victory
+movement = 1
